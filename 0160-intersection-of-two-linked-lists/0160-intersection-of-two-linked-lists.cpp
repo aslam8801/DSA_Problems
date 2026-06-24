00d7322726f4ed0bdda966ab6595aa25b74e45a8
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-06-24 12:20:25 +0530

    Time: 50 ms (88.85%), Space: 23.9 MB (56.08%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -13,20 +13,27 @@ public:
             return nullptr;
         }
 
-        unordered_map<ListNode*, int> mp;
-        ListNode* temp = headA;
-        while(temp){
-            mp[temp] = 1;
-            temp = temp->next;
-        }
-        temp = headB;
-        while(temp){
-            if(mp.find(temp) != mp.end()){
-                return temp;
-            }
-            temp = temp->next;
+        ListNode *l1 = headA, *l2 = headB;
+        if(l1 == l2){
+            return l1;
         }
 
-        return nullptr;
+        while(l1 != l2){
+
+            l1 = l1->next;
+            l2 = l2->next;
+            if(l1 == l2){
+                return l1;
+            }
+
+            if(l1 == nullptr){
+                l1 = headB;
+            }
+            if(l2 == nullptr){
+                l2 = headA;
+            }
+
+        }
+        return l1;
     }
 };
