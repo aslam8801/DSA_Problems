8ac79f111a21e148403b60461486cf99769b63e5
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-08-21 16:19:56 +0530

    Time: 0 ms (100%), Space: 19.7 MB (45.06%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -19,11 +19,11 @@ public:
         int ans = 0;
         queue<pair<TreeNode*, unsigned long long>> q;
         q.push({root, 0});
+
         while(!q.empty()){
             int sz = q.size();
             unsigned long long minIdx = q.front().second;
             unsigned long long first, last;
-            
             for(int i = 0; i < sz; i++){
                 unsigned long long currIdx = q.front().second - minIdx;
                 TreeNode* node = q.front().first;
@@ -39,7 +39,7 @@ public:
                     q.push({node->right, 2*currIdx+2});
                 }
             }
-            ans = max(ans, (int)(last-first+1));
+            ans = max(ans, (int)(last - first + 1));
         }
         return ans;
     }
