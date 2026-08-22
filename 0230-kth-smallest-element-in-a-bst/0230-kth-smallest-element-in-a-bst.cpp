1c993832d5f653f2b8d7fdb532c52bf6d5a9a032
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-08-22 18:56:43 +0530

    Time: 0 ms (100%), Space: 24.4 MB (40.9%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,41 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        if(root == nullptr){
+    int inorder(TreeNode* root, int& cnt, int k) {
+        if(root == nullptr) {
             return -1;
         }
 
-        priority_queue<int, vector<int>> pq;
+        // Left
+        int left = inorder(root->left, cnt, k);
 
-        TreeNode* curr = root;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* f = q.front();
-            q.pop();
-            pq.push(f->val);
-            while(pq.size() > k){
-                pq.pop();
-            }
-            if(f->left){
-                q.push(f->left);
-            }
-            if(f->right){
-                q.push(f->right);
-            }
+        if(left != -1) {
+            return left;
         }
 
-        return pq.top();
+        // Root
+        cnt++;
+
+        if(cnt == k) {
+            return root->val;
+        }
+
+        // Right
+        return inorder(root->right, cnt, k);
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        int cnt = 0;
+        return inorder(root, cnt, k);
     }
 };
