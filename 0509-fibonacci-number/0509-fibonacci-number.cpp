8b986641e5587d1ea86faaf6d0ba59cc75d2368a
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-08-22 23:03:53 +0530

    Time: 13 ms (31.38%), Space: 7.9 MB (38.75%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    int fibb(int n){
-        if(n == 0 || n == 1){
+    int fib(int n) {
+        if(n <= 1){
             return n;
         }
 
-        return fibb(n-1) + fibb(n-2);
-    }
-    int fib(int n) {
-        return fibb(n);
+        return fib(n-1) + fib(n-2);
     }
 };
