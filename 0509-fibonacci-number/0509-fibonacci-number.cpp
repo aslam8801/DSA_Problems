c26bd5f37dcc680ce52a9a22d5f3133263a98fac
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-09-11 00:32:19 +0530

    Time: 2 ms (77.34%), Space: 7.9 MB (38.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,15 +4,14 @@ public:
         if(n == 0){
             return 0;
         }
-        int dp[n+1];
-        memset(dp, -1, sizeof(dp));
 
-        dp[0] = 0;
-        dp[1] = 1;
+        int first = 0, sec = 1;
+
         for(int i = 2; i < n+1; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int ans = first + sec;
+            first = sec;
+            sec = ans;
         }
-
-        return dp[n];
+        return sec;
     }
 };
