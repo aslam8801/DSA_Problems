5c7c2d8cde4309a1b7e19893add0de0eda413726
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-09-12 02:33:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (53.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
-    int dp[101][101];
-    int solve(int sr, int sc, int m, int n){
-        if(sr == m-1 && sc == n-1){
-            return 1;
-        }
-        if(sr >= m || sc >= n){
-            return 0;
-        }
-        if(dp[sr][sc] != -1){
-            return dp[sr][sc];
+    int uniquePaths(int m, int n) {
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        dp[0][0] = 1;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(i == 0 && j == 0)
+                    continue;
+
+                int up = 0;
+                int left = 0;
+
+                if(i > 0)
+                    up = dp[i-1][j];
+
+                if(j > 0)
+                    left = dp[i][j-1];
+
+                dp[i][j] = up + left;
+            }
         }
 
-        return dp[sr][sc] = solve(sr+1, sc, m, n) + solve(sr, sc+1, m, n);
-    }
-    int uniquePaths(int m, int n) {
-        int sr = 0, sc = 0;
-        memset(dp, -1, sizeof(dp));
-        return solve(sr, sc, m, n);
+        return dp[m-1][n-1];
     }
 };
