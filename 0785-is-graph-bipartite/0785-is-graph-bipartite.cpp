a5fc1bed3359ddaa180b07d258b1047ab177754d
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-09-18 19:09:02 +0530

    Time: 0 ms (100%), Space: 17.6 MB (39.38%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -18,6 +18,31 @@ public:
         }
         return true;
     }
+
+    bool bfs(vector<vector<int>>& graph, int curr, vector<int>& color, int currColor){
+
+        queue<int>q;
+        q.push(curr);
+        color[curr] = currColor;
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int &v:graph[u]){
+                if(color[v] == color[u]){
+                    return false;
+                }
+
+                if(color[v] == -1){
+                    int colorV = 1 - color[u];
+                    q.push(v);
+                    color[v] = colorV;
+                }
+            }
+        }
+        return true;
+    }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int V = graph.size();
 
@@ -25,12 +50,13 @@ public:
 
         for(int i = 0; i < V; i++){
             if(color[i] == -1){
-                bool ans = dfs(graph, i, color, 1);
+                bool ans = bfs(graph, i, color, 1);
                 if(!ans){
                     return ans;
                 }
             }
         }
+
         return true;
     }
 };
