2b394a28e5317e9409be0a8bceee5e1af6495ef0
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-09-22 20:06:30 +0530

    Time: 36 ms (85.91%), Space: 150.9 MB (84.11%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,45 +1,62 @@
 class Solution {
 public:
-    void dfs(unordered_map<int, vector<int>>& adj, int u, vector<bool>& vis, int& cnt){
-        vis[u] = true;
-        cnt += 1;
-        for(auto &v:adj[u]){
-            if(!vis[v]){
-                dfs(adj, v, vis, cnt);
-            }
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x) {
+        if(parent[x] == x) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void unionT(int x, int y) {
+        int x_par = find(x);
+        int y_par = find(y);
+
+        if(x_par == y_par) {
+            return;
+        }
+
+        if(rank[x_par] > rank[y_par]) {
+            parent[y_par] = x_par;
+        }
+        else if(rank[x_par] < rank[y_par]) {
+            parent[x_par] = y_par;
+        }
+        else {
+            parent[y_par] = x_par;
+            rank[x_par]++;
         }
     }
+
     long long countPairs(int n, vector<vector<int>>& edges) {
-        unordered_map<int, vector<int>> adj;
 
-        for(auto &curr:edges){
-            int u = curr[0], v = curr[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        parent.resize(n);
+        rank.assign(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
         }
 
-        vector<bool>vis(n, false);
-        vector<int> ans;
+        for(auto &edge : edges) {
+            unionT(edge[0], edge[1]);
+        }
 
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                int cnt = 0;
-                dfs(adj, i, vis, cnt);
-                if(cnt != 0)
-                ans.push_back(cnt);
-            }
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < n; i++) {
+            mp[find(i)]++;
         }
-        if(ans.size() == 1){
-            return 0;
-        }
-        
+
         long long sum = 0;
-long long prev = 0;
+        long long prev = 0;
 
-for(int size : ans) {
-    sum += prev * size;
-    prev += size;
-}
+        for(auto &[key, size] : mp) {
+            sum += prev * size;
+            prev += size;
+        }
 
         return sum;
     }
