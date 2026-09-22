f7d24cf6ad6c66d362fd2acd6dafbe0543c71dc4
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-09-22 11:54:16 +0530

    Time: 29 ms (23.25%), Space: 52.6 MB (19.17%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> rank;
-    int find(int x){
-        if(x == parent[x]){
-            return x;
-        }
+    void dfs(vector<vector<int>>& connections, int u, vector<bool>& vis){
+        vis[u] = true;
 
-        return parent[x] = find(parent[x]);
-    }
-
-    void unionT(int x, int y){
-        int x_parent = find(x);
-        int y_parent = find(y);
-
-        if(rank[x_parent] > rank[y_parent]){
-            parent[y_parent] = x_parent;
-        }else if(rank[x_parent] < rank[y_parent]){
-            parent[x_parent] = y_parent;
-        }else{
-            parent[y_parent] = x_parent;
-            rank[x_parent] += 1;
+        for(auto &v:connections[u]){
+            if(!vis[v]){
+                dfs(connections, v, vis);
+            }
         }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size() < n-1){
             return -1;
         }
-
-        parent.resize(n);
-        rank.assign(n, 0);
-
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        }
-
-        for(auto &curr:connections){
-            unionT(curr[0], curr[1]);
-        }
-
         int component = 0;
+         vector<vector<int>> adj(n);
+
+        for(auto &edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<bool> vis(n, false);
 
         for(int i = 0; i < n; i++){
-            if(i == parent[i]){
+            if(!vis[i]){
+                dfs(adj, i, vis);
                 component += 1;
             }
         }
