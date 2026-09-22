dd12f11813318014fe38285cfd250ba165301e03
Aslam Kureshi <kureshiaslam176@gmail.com>
2026-09-22 20:43:25 +0530

    Time: 47 ms (68.48%), Space: 151 MB (72.51%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -3,58 +3,52 @@ public:
     vector<int> parent;
     vector<int> rank;
 
-    int find(int x) {
-        if(parent[x] == x) {
+    int find(int x){
+        if(x == parent[x]){
             return x;
         }
 
         return parent[x] = find(parent[x]);
     }
 
-    void unionT(int x, int y) {
+    void unionT(int x, int y){
         int x_par = find(x);
         int y_par = find(y);
 
-        if(x_par == y_par) {
-            return;
+        if(x_par == y_par){
+            return ;
         }
 
-        if(rank[x_par] > rank[y_par]) {
+        if(rank[x_par] > rank[y_par]){
             parent[y_par] = x_par;
-        }
-        else if(rank[x_par] < rank[y_par]) {
+        }else if(rank[x_par] < rank[y_par]){
             parent[x_par] = y_par;
-        }
-        else {
+        }else{
             parent[y_par] = x_par;
-            rank[x_par]++;
+            rank[x_par] += 1;
         }
     }
-
     long long countPairs(int n, vector<vector<int>>& edges) {
-
         parent.resize(n);
-        rank.assign(n, 0);
+        rank.resize(n, 0);
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             parent[i] = i;
         }
 
-        for(auto &edge : edges) {
-            unionT(edge[0], edge[1]);
+        for(auto &vec:edges){
+            int u = vec[0], v = vec[1];
+            unionT(u, v);
         }
 
         unordered_map<int, int> mp;
-
-        for(int i = 0; i < n; i++) {
-            mp[find(i)]++;
+        for(int i = 0; i < n; i++){
+            mp[find(i)]++; // why dont we are using parent[i]
         }
 
-        long long sum = 0;
-        long long prev = 0;
-
-        for(auto &[key, size] : mp) {
-            sum += prev * size;
+        long long sum = 0, prev = 0;
+        for(auto &[key, size]:mp){
+            sum += (prev*size);
             prev += size;
         }
 
